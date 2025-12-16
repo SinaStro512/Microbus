92b82029d3f621d1859b36e6c9dc04c3ce11f27d
SinaStro512 <jacques.fourny@gmx.fr>
2025-12-17 00:10:11 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,11 +6,12 @@ let grassOffset = 0;
 const character = document.getElementById('character')
 const content = document.getElementById("content");
 const grass = document.getElementById("grass");
+const bg = document.getElementById('bg');
 const interval = 200; //temps en millisecondes entre les grames d'animation
 const duration = 3000; //temps en millisecondes entre les slides
 const maxOffset = Math.round((1 - (content.offsetWidth / content.parentElement.offsetWidth)) * 100);
 const increment = 100; //in %, distance between each slide
-const bgIncrement = Math.round(100 / ((content.offsetWidth / content.parentElement.offsetWidth) - 1)); //distance the bg has to travel between slides
+const bgIncrement = -2 //distance the bg has to travel between slides
 const grassIncrement = 20; //distance the grass has to travel between slides
 
 const walkRightFrames = [
@@ -43,6 +44,7 @@ const runRightFrames = [
 
 content.style.transition = `margin-left ${duration}ms linear`;
 grass.style.transition = `margin-left ${duration}ms linear`;
+bg.style.transition = `margin-left ${duration}ms linear`;
 document.body.style.transition = `background-position ${duration}ms linear`;
 
 function animate(frames) {
@@ -68,17 +70,32 @@ function move(direction) {
     offset -= increment;
     grassOffset -= grassIncrement;
     bgOffset += bgIncrement;
-    console.log(bgOffset)
   } else if (direction === 'left') {
     offset += increment;
     grassOffset += grassIncrement;
     bgOffset -= bgIncrement;
-    console.log(bgOffset)
   }
-  content.style.marginLeft = offset + "%";
-  grass.style.marginLeft = grassOffset + "%";
-  document.body.style.backgroundPosition = bgOffset + "% 70%";
+  content.style.marginLeft = offset + '%';
+  grass.style.marginLeft = grassOffset + '%';
+  bg.style.marginLeft = bgOffset + '%';
 }
+
+// function move(direction) {
+//   if (direction === 'right') {
+//     offset -= increment;
+//     grassOffset -= grassIncrement;
+//     bgOffset += bgIncrement;
+//   } else if (direction === 'left') {
+//     offset += increment;
+//     grassOffset += grassIncrement;
+//     bgOffset -= bgIncrement;
+//   }
+  
+//   // Use transform instead of margin
+//   content.style.transform = `translateX(${offset}%)`;
+//   grass.style.transform = `translateX(${grassOffset}%)`;
+//   bg.style.transform = `translateX(${bgOffset}%)`;
+// }
 
 function stop() {
   clearInterval(isMoving);
